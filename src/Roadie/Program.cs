a9dac8a6ba0d95dccd2f5a5db36ee3ad6575a059
Aux <justin@akitaux.com>
2023-05-07 22:48:05 -0500

    Move service configuration to registry

--- src/Roadie/Program.cs
+++ src/Roadie/Program.cs
@@ -1,15 +1,14 @@
-using Discord;
-using Discord.Interactions;
-using Discord.Rest;
-using Discord.WebSocket;
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authorization;
-using Roadie.Authentication;
+using Roadie;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-ConfigureServices(builder.Services);
+builder.Services.AddRazorPages();
+builder.Services.Configure<RouteOptions>(options => options.LowercaseUrls = true);
+
+builder.AddAuthentication();
+builder.AddDiscord();
+builder.AddGumroad();
+builder.AddFirestore();
 
 var app = builder.Build();
 
@@ -33,56 +32,3 @@ app.MapRazorPages();
 app.MapControllers();
 
 app.Run();
-
-void ConfigureServices(IServiceCollection services)
-{
-    builder.Services.AddRazorPages();
-    builder.Services.Configure<RouteOptions>(options => options.LowercaseUrls = true);
-
-    // Auth services
-
-    builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-    .AddCookie("DiscordSession")
-    .AddCookie("GumroadSession")
-    .AddDiscord(options =>
-    {
-        builder.Configuration.GetSection("Discord").Bind(options);
-        options.SignInScheme = "DiscordSession";
-        options.SaveTokens = true;
-
-        options.Scope.Add("identify");
-        options.Scope.Add("guilds");
-    })
-    .AddGumroad(options =>
-    {
-        builder.Configuration.GetSection("Gumroad").Bind(options);
-        options.SignInScheme = "GumroadSession";
-        options.SaveTokens = true;
-
-        options.Scope.Add("view_profile");
-    });
-
-    builder.Services.AddAuthorization(options =>
-    {
-        options.DefaultPolicy = new AuthorizationPolicyBuilder("Discord", "Gumroad")
-            .RequireAuthenticatedUser()
-            .Build();
-    });
-
-    // Discord services
-
-    builder.Services.AddTransient<DiscordRestClient>();
-    var discordSocketClient = new DiscordSocketClient(new()
-    {
-        AlwaysDownloadUsers = false,
-        GatewayIntents = GatewayIntents.None,
-        LogLevel = LogSeverity.Verbose,
-        MessageCacheSize = 0,
-        SuppressUnknownDispatchWarnings = true
-    });
-    builder.Services.AddSingleton(discordSocketClient);
-    builder.Services.AddSingleton(new InteractionService(discordSocketClient, new()
-    {
-        LogLevel = LogSeverity.Verbose
-    }));
-}
---
+++ src/Roadie/Registry.cs
@@ -0,0 +1,82 @@
+﻿using Discord;
+using Discord.Interactions;
+using Discord.Rest;
+using Discord.WebSocket;
+using Google.Cloud.Firestore;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Roadie.Authentication;
+using RestEase.HttpClientFactory;
+using Roadie.Gumroad;
+using RestEase;
+
+namespace Roadie
+{
+    public static class Registry
+    {
+        public static void AddAuthentication(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+            .AddCookie("DiscordSession")
+            .AddCookie("GumroadSession")
+            .AddDiscord(options =>
+            {
+                builder.Configuration.GetSection("Discord").Bind(options);
+                options.SignInScheme = "DiscordSession";
+                options.SaveTokens = true;
+
+                options.Scope.Add("identify");
+                options.Scope.Add("guilds");
+            })
+            .AddGumroad(options =>
+            {
+                builder.Configuration.GetSection("Gumroad").Bind(options);
+                options.SignInScheme = "GumroadSession";
+                options.SaveTokens = true;
+
+                options.Scope.Add("view_profile");
+            });
+
+            builder.Services.AddAuthorization(options =>
+            {
+                options.DefaultPolicy = new AuthorizationPolicyBuilder("Discord", "Gumroad")
+                    .RequireAuthenticatedUser()
+                    .Build();
+            });
+        }
+
+        public static void AddDiscord(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddTransient<DiscordRestClient>();
+            builder.Services.AddSingleton(_ => new DiscordSocketClient(new()
+            {
+                AlwaysDownloadUsers = false,
+                GatewayIntents = GatewayIntents.None,
+                LogLevel = LogSeverity.Verbose,
+                MessageCacheSize = 0,
+                SuppressUnknownDispatchWarnings = true
+            }));
+            builder.Services.AddSingleton(services =>
+            {
+                var discord = services.GetRequiredService<DiscordSocketClient>();
+                return new InteractionService(discord, new()
+                {
+                    LogLevel = LogSeverity.Verbose
+                });
+            });
+        }
+
+        public static void AddGumroad(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddRestEaseClient<IGumroadApi>(GumroadConstants.ApiUrl, new()
+            {
+                RestClientConfigurer = client => client.ResponseDeserializer = new Gumroad.JsonResponseDeserializer()
+            });
+        }
+
+        public static void AddFirestore(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddSingleton(_ => FirestoreDb.Create(builder.Configuration["Firestore:ProjectId"]));
+        }
+    }
+}
